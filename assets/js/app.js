8fedef491a2afde550138168cd68bc5f00508743
Lagmay <quickfire@uspicorp.com>
2018-03-09 15:26:31 -0600

    test scripts for firebase read and write data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,45 @@
 var db = firebase.database();
 
 
+var roomNumber = 0;
+var playerId = 0;
+
+gameState();
 function writeUser(roomId, name,email, player){
     db.ref('/game/'+roomId+"/"+player).set({name: name, email: email});
+    gameState();
 }
 
 
-writeUser(1, "john", "jj@js.com", 1); 
-function readData(id){
+// writeUser(1, "john", "jj@js.com", 1); 
+function gameState(){
     var data = firebase.database().ref('/game');
+    let value
     data.on('value', function(d){
-        d.val().forEach((element, i) => {
-          element.forEach((e, j)=>{
-              console.log(JSON.stringify(e) + "index: " + j)
-          })
-        });
+        // d.val().forEach((element, i) => {
+        //   element.forEach((e, j)=>{
+        //       console.log(JSON.stringify(e) + "index: " + j)
+        //   })
+        // });
+        if(!d.val()){
+            roomNumber = 1;
+            player = 1;
+            writeUser(roomNumber, "john", "ema", player);
+        }else if(d.val().length < 2){
+            roomNumber = d.val().length -1;
+            player = 2;
+            writeUser(roomNumber, "mike", "ppp", player);
+            
+        }
+        console.log(d.val());
     })
+
 }
 
-readData(1);
+$('button').on('click', ()=>{ 
+    writeUser(2, "john", "jj@js.com", 1); 
+})
+
+function clearGame(){
+    db.ref('/game').set("");
+}
