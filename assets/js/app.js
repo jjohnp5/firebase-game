0bd0968a4503e28c4f4a0d7c16de63e4d5a23eb6
John <jjohnp5@gmail.com>
2018-03-10 13:57:11 -0600

    added a pseudo db schema. updated js and html to start a chat box. Chat box is now properly working.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,101 @@
 var db = firebase.database();
 
+var name = "";
+var email = "";
+var gameReady = false;
 
-var roomNumber = 0;
-var playerId = 0;
+var roomNumber;
+var player;
+var wins = 0;
 
-gameState();
-function writeUser(roomId, name,email, player){
-    db.ref('/game/'+roomId+"/"+player).set({name: name, email: email});
-    gameState();
+function writeUser(roomId, name,email, player, wins){
+    db.ref(`/game/${roomId}/${player}`).set({name: name, email: email, wins: wins});
+
 }
+
+function chooseRPS(roomId, player, choice){
+    db.ref(`/game/${roomId}/${player}/choice`).set(choice);
+
+}
+function setTurn(roomId, turn){
+    db.ref(`/game/${roomId}/turn`).set(turn);
+
+}
+function setWins(roomId, player, wins){
+    db.ref(`/game/${roomId}/${player}/wins`).set(wins);
+
+}
+function resetWins(roomId, player, wins){
+    db.ref(`/game/${roomId}/${player}/wins`).set(0);
+}
+
 
 
 // writeUser(1, "john", "jj@js.com", 1); 
 function gameState(){
-    var data = firebase.database().ref('/game');
-    let value
-    data.on('value', function(d){
+    firebase.database().ref('/game').once('value').then(function(d){
         // d.val().forEach((element, i) => {
         //   element.forEach((e, j)=>{
         //       console.log(JSON.stringify(e) + "index: " + j)
         //   })
         // });
-        if(!d.val()){
-            roomNumber = 1;
-            player = 1;
-            writeUser(roomNumber, "john", "ema", player);
-        }else if(d.val().length < 2){
-            roomNumber = d.val().length -1;
-            player = 2;
-            writeUser(roomNumber, "mike", "ppp", player);
+        console.log(d.key);
+        
+        if(!gameReady && !player && !roomNumber){
+            if(!d.val()){
+                roomNumber = 1;
+                player = 1;
+                writeUser(roomNumber, name, email, player, wins);
+                gameReady = true;
+                $('.registration').remove();
+                var chats = firebase.database().ref(`/game/${roomNumber}/chat`);
+                    chats.on('child_added', function(chat){
+                        $('.chatbox').append($('<p class="chat-message">').text(`${chat.val().name}: ${chat.val().message}`))
+                });
+            }else if(d.val() && d.val()[d.val().length-1].length < 3){
+                roomNumber = d.val().length -1;
+                player = 2;
+                writeUser(roomNumber, name, email, player, wins);
+                gameReady = true;
+                $('.registration').remove();
+                setTurn(roomNumber, 1);
+                var chats = firebase.database().ref(`/game/${roomNumber}/chat`);
+                    chats.on('child_added', function(chat){
+                        $('.chatbox').append($('<p class="chat-message">').text(`${chat.val().name}: ${chat.val().message}`))
+                });
+                
+            }
             
         }
-        console.log(d.val());
     })
 
 }
 
-$('button').on('click', ()=>{ 
-    writeUser(2, "john", "jj@js.com", 1); 
+
+$('.clear').on('click', ()=>{ 
+    clearGame();
+
 })
+
+$('button[type="submit"]').on('click', (e)=>{
+    e.preventDefault();
+    name = $('#name').val();
+    email = $('#email').val();
+    gameState();
+    
+    
+})
+
+$('.send').on('click', function(e){
+    e.preventDefault();
+    const updateChat = firebase.database().ref(`/game/${roomNumber}/chat`);
+    var push = updateChat.push();
+    push.set({name: name, message: $('.message').val()});
+    $('.message').val("");
+    
+    
+})
+
 
 function clearGame(){
     db.ref('/game').set("");
