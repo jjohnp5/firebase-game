6db5d3ddf21245e40d6d72d12609a9e6602c1802
Lagmay <quickfire@uspicorp.com>
2018-03-13 12:03:34 -0500

    logic for RPS fully set up. Need to set up a queue system that will add users to different available rooms once they join.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,22 @@
-var db = firebase.database();
+let db = firebase.database();
 
-var name = "";
-var email = "";
-var gameReady = false;
+let name = "";
+let email = "";
+let gameReady = false;
 
-var roomNumber;
-var player;
-var enemy;
-var wins = 0;
+let roomNumber;
+let player;
+let enemy;
+let wins = 0;
 
-var game = {
-    choices: ["rock", "paper", "scissors"],
-    player: player,
-
-}
+let choice = "";
 
 function setName(id, name){
     $(`.name-${id}`).text(name);
 }
 
 function writeUser(roomId, name,email, player, wins){
-    db.ref(`/game/${roomId}/${player}`).set({name: name, email: email, wins: wins});
+    db.ref(`/game/${roomId}/${player}`).set({name: name, email: email, wins: wins, choice: ""});
 
 }
 
@@ -45,7 +41,7 @@ function resetWins(roomId, player, wins){
 function gameState(){
     firebase.database().ref('/game').once('value').then(function(d){
 
-        
+        console.log(d.val())
 
         
         if(!gameReady && !player && !roomNumber){
@@ -56,26 +52,27 @@ function gameState(){
                 writeUser(roomNumber, name, email, player, wins);
                 gameReady = true;
                 $('.registration').remove();
+                $(`.player-${player} .choices`).removeClass("display");
                 var chats = firebase.database().ref(`/game/${roomNumber}/chat`);
                     chats.on('child_added', function(chat){
                         $('.chatbox').append($('<p class="chat-message">').text(`${chat.val().name}: ${chat.val().message}`))
                 });
                 setName(player, name);
-                firebase.database().ref(`/game/${roomNumber}`).on('value', function(d){
-                    console.log(d.val()[roomNumber]);
-                    if(d.val()[enemy].hasOwnProperty("name")){
-                        setName(enemy, d.val()[enemy].name)
-                    }
-                    if(d.val().turn === 0){
-                        $('.turn').text(d.val().turn)
-                    }else if(d.val().turn === player){
-                        $('.turn').text(d.val()[player].name)
-                    }else if(d.val().turn === enemy){
-                        $('.turn').text(d.val()[enemy].name)
-                    }
-                    console.log(d.val());
+                // firebase.database().ref(`/game/${roomNumber}`).on('value', function(d){
+                //     console.log(d.val()[roomNumber]);
+                //     if(d.val()[enemy].hasOwnProperty("name")){
+                //         setName(enemy, d.val()[enemy].name)
+                //     }
+                //     if(d.val().turn === 0){
+                //         $('.turn').text(d.val().turn)
+                //     }else if(d.val().turn === player){
+                //         $('.turn').text(d.val()[player].name)
+                //     }else if(d.val().turn === enemy){
+                //         $('.turn').text(d.val()[enemy].name)
+                //     }
+                //     console.log(d.val());
                 
-                })
+                // })
                 
             }else if(d.val() && d.val()[d.val().length-1].length < 3){
                 roomNumber = d.val().length -1;
@@ -84,85 +81,169 @@ function gameState(){
                 writeUser(roomNumber, name, email, player, wins);
                 gameReady = true;
                 $('.registration').remove();
-                setTurn(roomNumber, 1);
+                $(`.player-${player} .choices`).removeClass("display");
                 var chats = firebase.database().ref(`/game/${roomNumber}/chat`);
                     chats.on('child_added', function(chat){
                         $('.chatbox').append($('<p class="chat-message">').text(`${chat.val().name}: ${chat.val().message}`))
                 });
                 setName(player, name);
-                firebase.database().ref(`/game/${roomNumber}`).on('value', function(d){
-                    console.log(d.val()[roomNumber]);
-                    if(d.val()[enemy].hasOwnProperty("name")){
-                        setName(enemy, d.val()[enemy].name)
-                    }
-                    if(d.val().turn === 0){
-                        $('.turn').text(d.val().turn)
-                    }else if(d.val().turn === enemy){
-                        $('.turn').text(d.val()[enemy].name)
-                    }else if(d.val().turn === player){
-                        $('.turn').text(d.val()[player].name)
-                    }
-                    console.log(d.val());
+                setTurn(roomNumber, 1);
+                // firebase.database().ref(`/game/${roomNumber}`).on('value', function(d){
+                //     console.log(d.val()[roomNumber]);
+                //     if(d.val()[enemy].hasOwnProperty("name")){
+                //         setName(enemy, d.val()[enemy].name)
+                //     }
+                //     if(d.val().turn === 0){
+                //         $('.turn').text(d.val().turn)
+                //     }else if(d.val().turn === enemy){
+                //         $('.turn').text(d.val()[enemy].name)
+                //     }else if(d.val().turn === player){
+                //         $('.turn').text(d.val()[player].name)
+                //     }
+                //     console.log(d.val());
                 
-                })
-            }else{
+                // })
+            }else if(d.val() && d.val().length >= 2 && d.val()[d.val().length-1].hasOwnProperty('1') && !d.val()[d.val().length-1].hasOwnProperty('2')){
+                roomNumber = d.val().length -1;
+                player = 2;
+                enemy = 1;
+                writeUser(roomNumber, name, email, player, wins);
+                gameReady = true;
+                $('.registration').remove();
+                $(`.player-${player} .choices`).removeClass("display");
+                var chats = firebase.database().ref(`/game/${roomNumber}/chat`);
+                    chats.on('child_added', function(chat){
+                        $('.chatbox').append($('<p class="chat-message">').text(`${chat.val().name}: ${chat.val().message}`))
+                });
+                setName(player, name);
+                setTurn(roomNumber, 1);
+                // firebase.database().ref(`/game/${roomNumber}`).on('value', function(d){
+                //     console.log(d.val()[roomNumber]);
+                //     if(d.val()[enemy].hasOwnProperty("name")){
+                //         setName(enemy, d.val()[enemy].name)
+                //     }
+                //     if(d.val().turn === 0){
+                //         $('.turn').text(d.val().turn)
+                //     }else if(d.val().turn === enemy){
+                //         $('.turn').text(d.val()[enemy].name)
+                //     }else if(d.val().turn === player){
+                //         $('.turn').text(d.val()[player].name)
+                //     }
+                //     console.log(d.val());
+                
+                // })
+                console.log('2 players currently playing');
+            }else if(d.val() && d.val().length >= 2 && !d.val()[d.val().length]){
+                roomNumber = d.val().length;
+                player = 1;
+                enemy = 2;
+                writeUser(roomNumber, name, email, player, wins);
+                gameReady = true;
+                $('.registration').remove();
+                $(`.player-${player} .choices`).removeClass("display");
+                var chats = firebase.database().ref(`/game/${roomNumber}/chat`);
+                    chats.on('child_added', function(chat){
+                        $('.chatbox').append($('<p class="chat-message">').text(`${chat.val().name}: ${chat.val().message}`))
+                });
+                setName(player, name);
+                
                 console.log('2 players currently playing');
             }
+            firebase.database().ref(`/game/${roomNumber}`).on('value', function(d){
+                if(d.val()[enemy].hasOwnProperty("name")){
+                    setName(enemy, d.val()[enemy].name)
+                }
+                
+                switch(d.val()[enemy].choice){
+                    case "rock":
+                        if(d.val()[player].choice === "rock"){
+                            console.log("draw");
+                            chooseRPS(roomNumber, player, "");
+                        }else if(d.val()[player].choice === "paper"){
+                            console.log("lose");
+                            chooseRPS(roomNumber, player, "");
+                        }else if(d.val()[player].choice === "scissors"){
+                            console.log("win");
+                            chooseRPS(roomNumber, player, "");
+                            setWins(roomNumber, player, d.val()[player].wins += 1)
+                        }
+                        break;
+                    case "paper":
+                        if(d.val()[player].choice === "rock"){
+                            console.log("win");
+                            chooseRPS(roomNumber, player, "");
+                            setWins(roomNumber, player, d.val()[player].wins += 1)
+                        }else if(d.val()[player].choice === "paper"){
+                            console.log("draw");
+                            chooseRPS(roomNumber, player, "");
 
-            // }else if(d.val() && (!d.val()[d.val().length-1].hasOwnProperty('1') && !d.val()[d.val().length-1].hasOwnProperty('2'))){
-            //     roomNumber = d.val().length;
-            //     console.log(roomNumber);
-            //     player = 1;
-            //     enemy = 2;
-            //     writeUser(roomNumber, name, email, player, wins);
-            //     gameReady = true;
-            //     $('.registration').remove();
-            //     var chats = firebase.database().ref(`/game/${roomNumber}/chat`);
-            //         chats.on('child_added', function(chat){
-            //             $('.chatbox').append($('<p class="chat-message">').text(`${chat.val().name}: ${chat.val().message}`))
-            //     });
-            // }else if(d.val() && !d.val()[d.val().length-1].hasOwnProperty('1') && d.val()[d.val().length-1].hasOwnProperty('2')){
-            //     roomNumber = d.val().length;
-            //     console.log(roomNumber);
-            //     player = 1;
-            //     enemy = 2;
-            //     writeUser(roomNumber, name, email, player, wins);
-            //     gameReady = true;
-            //     $('.registration').remove();
-            //     var chats = firebase.database().ref(`/game/${roomNumber}/chat`);
-            //         chats.on('child_added', function(chat){
-            //             $('.chatbox').append($('<p class="chat-message">').text(`${chat.val().name}: ${chat.val().message}`))
-            //     });
-            // }else if(d.val() && d.val()[d.val().length-1].hasOwnProperty('1') && !d.val()[d.val().length-1].hasOwnProperty('2')){
-            //     roomNumber = d.val().length;
-            //     console.log(roomNumber);
-            //     player = 2;
-            //     enemy = 1;
-            //     writeUser(roomNumber, name, email, player, wins);
-            //     gameReady = true;
-            //     $('.registration').remove();
-            //     var chats = firebase.database().ref(`/game/${roomNumber}/chat`);
-            //         chats.on('child_added', function(chat){
-            //             $('.chatbox').append($('<p class="chat-message">').text(`${chat.val().name}: ${chat.val().message}`))
-            //     });
-            // }
+                        }else if(d.val()[player].choice === "scissors"){
+                            console.log("lose");
+                            chooseRPS(roomNumber, player, "");
+
+                        }
+                        break;
+                    case "scissors":
+                        if(d.val()[player].choice === "rock"){
+                            console.log("lose");
+                            chooseRPS(roomNumber, player, "");
+
+                        }else if(d.val()[player].choice === "paper"){
+                            console.log("win");
+                            chooseRPS(roomNumber, player, "");
+                            setWins(roomNumber, player, d.val()[player].wins += 1)
+                        }else if(d.val()[player].choice === "scissors"){
+                            console.log("draw");
+                            chooseRPS(roomNumber, player, "");
+
+                        }
+                        break;
+                }
+
+                
+                
+            
+            });
+            firebase.database().ref(`/game/${roomNumber}`).on('value', function(d){
+                
+                if(d.val().turn === 0){
+                    $('.turn').text(d.val().turn)
+                }else if(d.val().turn === enemy){
+                    $('.turn').text(d.val()[enemy].name)
+                }else if(d.val().turn === player){
+                    $('.turn').text(d.val()[player].name)
+                }
+
+                if(d.val().turn != player){
+                    $(`.player-${player} button`).attr("disabled", "disabled");
+                }else{
+                    $(`.player-${player} button`).removeAttr("disabled");
+
+                }
+            });
+
             
         }
+        
     })
 
 }
-firebase.database().ref(`/game/${roomNumber}`).on('value', function(d){
-    console.log(d.val()[roomNumber]);
-    if(d.val()[enemy].hasOwnProperty("name")){
-        setName(enemy, d.val()[roomNumber][enemy].name)
-    }
-    console.log(d.val());
-
-})
 
 
 $('.clear').on('click', ()=>{ 
     clearGame();
+
+})
+
+$('.choices button').on('click', function(){
+    let choice = $(this).data('id');
+    chooseRPS(roomNumber, player, choice);
+    if(player === 1){
+        setTurn(roomNumber, 2);
+    }else{
+        setTurn(roomNumber, 1);
+    }
+    // if()
 
 })
 
